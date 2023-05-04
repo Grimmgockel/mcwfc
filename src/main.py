491e73ba604c525573a082d6a0b5ea4482b238b0
Grimmgockel <justinhoffmann2000@googlemail.com>
2023-05-04 16:23:38 +0200

    add wfc structure

--- src/main.py
+++ src/main.py
@@ -2,10 +2,11 @@ from world import World
 from world import Realm
 
 # TODO complex get and set block examples
-# TODO handle different spawns
+# TODO chunk access
+# TODO handle different spawns, use chat client for chat commands
 if __name__ == '__main__':
-    MC_PATH = "../../../AppData/Roaming/.minecraft/saves/"
-    world = World(MC_PATH + "test_anvil/", "java 1.17.0")
+    MC_PATH = "../../../.minecraft/saves/"
+    world = World(MC_PATH + "test_anvil", "java 1.17.0")
 
     block, block_entity = world.get_block((-200, 4, 86), Realm.OVERWORLD)
 
---
+++ src/wfc/preprocessing.py
@@ -0,0 +1,14 @@
+class Preprocessor:
+    pass
+
+class VoxelTiledModel:
+    pass
+
+class PixelTiledModel:
+    pass
+
+class PixelOverlappingModel:
+    pass
+
+class VoxelOverlappingModel:
+    pass
